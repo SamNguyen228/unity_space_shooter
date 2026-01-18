083169bd19a62085075d63fa28be943ef71bd02a
SamNguyen228 <duonghoangsamet@gmail.com>
2026-01-18 21:06:13 +0700

    chore: initial Unity project setup (#1)

---
+++ Assets/Scripts/Blinking.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Blinking : MonoBehaviour
+{
+    private SpriteRenderer spriteRenderer;
+    public float blinkTime = 0.2f;
+
+    void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(Blink());
+    }
+
+    IEnumerator Blink()
+    {
+        while (true)
+        {
+            spriteRenderer.enabled = !spriteRenderer.enabled;
+            yield return new WaitForSeconds(blinkTime);
+        }
+    }
+}
